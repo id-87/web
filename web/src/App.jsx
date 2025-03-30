2c107345f319227b7f0d10c2721330b62301a330
[Harshit Gupta] <[hrstgpt87@gmail.com]>
2025-03-31 04:20:08 +0530

    Setting up the project

---
+++ web/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+function Text({display}){
+  return (
+    <div>
+      <p>{display}</p>
+    </div>
+  )
+}
+
+export default App
