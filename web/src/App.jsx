5e49c87106785a156f565ab18bff11ffdf5b2b3f
[Harshit Gupta] <[hrstgpt87@gmail.com]>
2025-03-31 06:16:20 +0530

    Tracking

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import MovieCard from './components/MovieCard'
+// import MovieCard from './components/MovieCard'
+import Home from './components/pages/Home'
 
 const App = () => {
+  const movieNumber=4
   return (
     <>
-    <MovieCard movie={{title: "Tim's Film", release_date: 2025}}/>
-    <MovieCard movie={{title: "Joe's Film", release_date: 2023}}/>
+    {/* conditional rendering */}
+    {movieNumber ===1 ? (<MovieCard movie={{title: "Tim's Film", release_date: 2025}}/>) : (<MovieCard movie={{title: "Joe's Film", release_date: 2023}}/>)}
+    {/* <Home/> */}
+    
     
     </>
   )
---
+++ web/src/components/pages/Home.jsx
@@ -0,0 +1,16 @@
+import MovieCard from "../components/MovieCard"
+
+function Home(){
+    movies=[
+        {id:1, title: "John Wick", release_date:2020},
+        {id:2, title: "Terminator", release_date:1998},
+        {id:3, title: "Matrix", release_date:1999}
+    ]
+    return <div className="home">
+        <div className="movies-grid">
+            {movies.map(movie => <MovieCard movie={movie} key={movie.id}/>)}
+        </div>
+    </div>
+}
+
+export default Home
