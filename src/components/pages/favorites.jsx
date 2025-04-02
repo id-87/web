c146f2e906ea0d9cfb25ef5f56067b12f1bce257
[Harshit Gupta] <[hrstgpt87@gmail.com]>
2025-04-02 13:03:45 +0530

    Styling fav

--- src/components/pages/favorites.jsx
+++ src/components/pages/favorites.jsx
@@ -1,7 +1,7 @@
 import '../../css/Favorites.css'
 
 function Favouraites(){
-    return <div className="favouraites-empty">
+    return <div className="favorites-empty">
         <h2>No favouraite Movies</h2>
         <p>Start adding movies and they appear here</p>
     </div>
