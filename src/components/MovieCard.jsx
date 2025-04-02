87a4d4aee9f8fb27f5c5d9305b38e23016fb5649
[Harshit Gupta] <[hrstgpt87@gmail.com]>
2025-04-03 00:19:55 +0530

    Linking images and formatting release date

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,13 +7,13 @@ function MovieCard({movie}){
 
     return <div className="movie-card">
         <div className="mpvoe-poster">
-            <img src={movie.url} alt={movie.title} />
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
             <div className="movie-overlay">
                 <button className="Like-button" onClick={onLike}> Like </button>
             </div>
             <div className="movie-info">
                 <h3>{movie.title}</h3>
-                <p>{movie.release_date}</p>
+                <p>{movie.release_date?.split("-")[0]}</p>
             </div>
         </div>
 
