6ba3c58a216d79bd28a2a832ca1724a60f0d18c5
Vivek W <92390419+AryanVBW@users.noreply.github.com>
2025-04-08 23:52:53 +0530

    fix: Fix favorites button functionality

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,7 +10,7 @@ function MovieCard({movie}){
     }
 
     return <div className="movie-card">
-        <div className="mpvoe-poster">
+        <div className="movie-poster">
             <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
             <div className="movie-overlay">
                 <button className={`favorite-btn ${favorite ? "active" : " "}`} onClick={onFavoriteClick}> ♥ </button>
